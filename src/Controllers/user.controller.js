bb443aabd997b9d24e4c2a2d1270a22776ebfefa
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-12-07 21:03:42 +0600

    - added user login register logout controllers
    - added user routes

---
+++ src/Controllers/user.controller.js
@@ -0,0 +1,123 @@
+import dotenv from 'dotenv';
+import { User } from '../Models/user.model';
+import { ApiError } from '../Utils/apiError';
+import { ApiResponse } from '../Utils/ApiResponse';
+import { asyncHandler } from '../Utils/AsyncHandler';
+
+
+dotenv.config({ path: './.env' })
+
+async function generateAccessAndRefreshToken(userId) {
+    try {
+        const user = await User.findById(userId);
+        if (!user) throw new ApiError(404, "User not found");
+
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { $set: { refreshToken } },
+            { new: true, validateBeforeSave: false }
+        );
+
+        return { accessToken, refreshToken };
+    } catch (err) {
+        throw new ApiError(500, `Token generation failed: ${err.message}`);
+    }
+}
+
+export const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password, image } = req.body
+    if (!name || !email || !password) {
+        throw new ApiError(400, 'Name, email and password are required fields')
+    }
+    if (await User.findOne({ email })) {
+        throw new ApiError(409, 'User already exists')
+    }
+    try {
+        await User.create({
+            name, email, password, image
+        })
+        res.status(200).json(
+            new ApiResponse(200, { message: 'User registered successfully' }, 'User registered successfully')
+        )
+    } catch (error) {
+        throw new ApiError(500, `Unable to register user due to ${error}`)
+    }
+})
+
+export const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw new ApiError(400, 'Email and password are required')
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new ApiError(404, 'User not found')
+    }
+
+    const isPasswordValid = await user.isPassCorrect(password)
+    if (!isPasswordValid) {
+        throw new ApiError(401, 'Invalid credentials')
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+
+    const updatedUser = await User.findById(user._id).select('-password -refreshToken')
+
+    const cookieOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict'
+    }
+
+    return res
+        .status(200)
+        .cookie('accessToken', accessToken, cookieOptions)
+        .cookie('refreshToken', refreshToken, cookieOptions)
+        .json(
+            new ApiResponse(200,
+                {
+                    user: updatedUser,
+                    accessToken,
+                    refreshToken
+                },
+                'Login successful'
+            )
+        )
+})
+
+export const logoutUser = asyncHandler(async (req, res) => {
+    const user = req.user
+    if (!user) {
+        throw new ApiError(400, 'Unable to find the user from tokens')
+    }
+    await User.findByIdAndUpdate(
+        user._id,
+        {
+            $unset: {
+                refreshToken: 1
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const cookieOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict'
+    }
+
+    return res
+        .status(200)
+        .clearCookie('accessToken', cookieOptions)
+        .clearCookie('refreshToken', cookieOptions)
+        .json(
+            new ApiResponse(200, { message: 'User logged out' }, 'User logout successfully')
+        )
+})
